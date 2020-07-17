d816f9196345f9cfa5ec838563b81454af79652e
brycealbertazzi <yourgitemail@example.com>
2020-07-16 21:42:37 -0700

    Removed initla route

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.use(express.json({
     extended: false
 }));
 
-app.get('/', (req, res) =>
-    res.json({
-        message: 'Welcome to the ContactKeeper API!'
-    })
-);
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
